32e34b1d3a9bc507a9a67e9c487dac0e6294512a
subha <siddharths2710@yahoo.com>
2021-03-19 16:55:02 +0530

    Added encryptor

--- util.py
+++ util.py
@@ -1,4 +1,9 @@
+import os
 import zipfile
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import (
+Cipher, algorithms, modes
+)
 
 class PwdZipFile(zipfile.ZipFile):
     def __init__(self, infile='', outfile='', mode='r'):
@@ -7,3 +12,21 @@ class PwdZipFile(zipfile.ZipFile):
                 file = self._file, mode=mode, 
                 compression=zipfile.ZIP_DEFLATED, 
                 compresslevel=9, allowZip64=True)
+
+class Encryptor:
+    def __init__(self):
+        self._key = os.urandom(32)
+        self._iv = os.urandom(64)
+        self._cipher = Cipher(
+                algorithms.AES(self._key),
+                modes.GCM(self._iv),
+                default_backend()
+            )
+    def encrypt(self, content):
+        enc = self._cipher.encryptor()
+        plain_text = self._key + b"||" \
+                     + content + b"||" \
+                     + self._iv
+        cipher_text = enc.update(plain_text) \
+                      + enc.finalize()
+        return cipher_text
