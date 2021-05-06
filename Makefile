7e723d4935e25eeb8f1361bc37691a5f66a7f176
subha <siddharths2710@yahoo.com>
2021-05-06 13:39:17 +0530

    Added Makefile

---
+++ Makefile
@@ -0,0 +1,17 @@
+.PHONY: install clean distclean TAGS info
+
+install:
+	pip3 install -r sec_vault/requirements/test.txt
+
+test:
+	pytest
+
+distclean:
+	rm -rf dist/
+	rm -rf build/
+	rm -rf *.snap
+	rm sec_vault/sec_vault.egg-info
+
+clean:
+	python setup.py clean --all
+	find . -name "*.pyc" -exec rm -rf {} \;
--- sec_vault/tests/test_factory.py
+++ sec_vault/tests/test_factory.py
@@ -1,15 +1,17 @@
 import os
 import pytest
 
-
-#>>> import cipherfactory
-#>>> import parser
-#>>> p=parser.CLIParser()
-#>>> a=p.parse_args()>>> c=cipherfactory.CipherFactory()
-#>>> c.load_cmd_cfg(parse_obj=a)>>> c.create_vault()
+import core.parser
+import core.cipherfactory
 
 @pytest.fixture
 def create_args():
     return ["--vault-file", "sec.vault", "--cipher-suite", "crypto_backend",  "--record-type", "login", "--create-vault"]
 
 def test_create_vault(create_args):
+    p = core.parser.CLIParser()
+    args = p.parse_args(create_args)
+    c = core.cipherfactory.CipherFactory()
+    c.load_cmd_cfg(parse_obj=args)
+    c.create_vault()
+    assert os.path.exists("sec.vault")
