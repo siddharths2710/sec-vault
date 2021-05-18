9eb45257aa1371dfe8b43fb77b3b2db777ca7e5c
subha <siddharths2710@yahoo.com>
2021-05-18 23:04:36 +0530

    Modified licenses and attribution

--- sec_vault/core/__init__.py
+++ sec_vault/core/__init__.py
@@ -1,7 +1,29 @@
-"""sec-vault driver module"""
-__author__ = "siddharths2710"
 import sys
 import logging
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "sec-vault"
+__summary__ = (
+   "SWISS Army-knife password management toolkit"             
+)
+__uri__ = "https://github.com/siddharths2710/sec-vault"
+__version__ = "0.0.1"
+__author__ = "Siddharth Srinivasan"
+__email__ = "siddharths2710@yahoo.com"
+__license__ = "LGPL-2.1 License"
+__copyright__ = "Copyright 2021 {0}".format(__author__)
+
 
 logging.basicConfig(
     format="%(levelname)s %(message)s",
